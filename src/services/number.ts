e9e0dcbd77405b091eb60697198ab48f91f430cc
hgovra <hgovra@gmail.com>
2022-08-23 15:36:33 -0300

    Ocultando URL da API em .env.

--- src/services/number.ts
+++ src/services/number.ts
@@ -9,7 +9,7 @@ axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 const api = axios.create({
-    baseURL: `https://us-central1-ss-devops.cloudfunctions.net`,
+    baseURL: process.env.REACT_APP_NUMBER_API,
 });
 
 interface ResultData {
